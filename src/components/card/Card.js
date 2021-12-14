e088b30f69b55fbb1b2b7470d0f09d673e93a6a3
mustafa <mltm0204@gmail.com>
2021-12-14 17:23:56 +0300

    ...

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,45 @@
 import React from 'react'
 
-const Card = ({toggleShow}) => {
+const Card = ({toggleShow, isVisible, categories}) => {
     return (
-        <div>
-            
-        </div>
+        <div className="card-container" >
+        {categories.map((card, index) => {
+          return (
+          <div className="cards" key={index} onClick={toggleShow}>
+                {
+                    isVisible 
+                      ? 
+                    <div>
+                        <img src={categories.img} alt=""/>
+                        <p>{categories.name}</p> 
+                    </div>
+                        :
+                    <div >
+                        <p>{categories.options} </p>
+                    </div>
+                }
+    
+                  
+          </div>
+          );
+        })}
+      </div>
     )
 }
 
-export default Card
+export default Card;
+
+// <div onClick={toggleShow}>
+//         {
+//             isVisible 
+//               ? 
+//             <div>
+//               <img src={categories.img} alt=""/>
+//               <p>{categories.name}</p> 
+//             </div>
+//             :
+//             <div >
+//               <p>{categories.options} </p>
+//             </div>
+//           }
+//         </div>
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,7 +1,9 @@
 import "./Cards.css";
 import { categories } from "../../helpers/Data";
 import { useState } from "react"
+import Card from "../card/Card";
 // console.log(categories);
+
 
 const Cards = ({toggleShow, setIsvisible, isVisible}) => {
   // const handleClick = (e)=>{
@@ -11,28 +13,15 @@ const Cards = ({toggleShow, setIsvisible, isVisible}) => {
     
     <div className="container">
       <h1>Languages</h1>
-      <div className="card-container" >
-        {categories.map((card, index) => {
-          return (
-          <div className="cards" key={index} onClick={()=>setIsvisible(!isVisible)} >
-   
-                  {
-                    isVisible 
-                      ? 
-                    <div>
-                      <img src={card.img} alt=""/>
-                      <p>{card.name}</p> 
-                    </div>
-                    :
-                    <div >
-                      <p>{card.options} </p>
-                    </div>
-                  }
-          </div>
-          );
-        })}
-      </div>
+      <Card 
+        toggleShow={toggleShow}
+        isVisible = {isVisible}
+        categories = {categories}
+      />
+       
     </div>
   );
 };
 export default Cards;
+
+// onClick={()=>setIsvisible(!isVisible)}
