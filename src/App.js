7dfe0580eb9ccf240307d83f34d56a4eb0e08fc6
mustafa <mltm0204@gmail.com>
2021-12-15 10:36:15 +0300

    react svg is added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Card from "./components/card/Card";
 import Data from "./components/card/Data";
 import "./App.css";
+import svg from "./assets/react.svg";
+
 function App() {
   return (
     <div className="App">
       <img
-        src="/static/media/react.7b78f5c3.svg"
+        src= {svg}
         alt="logo"
         id="logo"
       ></img>
